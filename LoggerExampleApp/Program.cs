56f50aa2e36de6106f03f80b42e6d24cfa053d86
s.szematowicz <s.szematowicz@gmail.com>
2022-04-01 17:33:59 +0200

    initial project

---
+++ LoggerExampleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+Debug.WriteLine("Hello, World!");
+
+public enum LogLevel
+{
+    Trace =	0,
+    Debug	= 1,
+    Information =	2,
+    Warning	= 3,
+    Error	= 4	,
+    Critical  = 5,
+    None	= 6,
+}
+
+
