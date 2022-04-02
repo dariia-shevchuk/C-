621eb9235d780ec270da53ee282f56576b395149
s.szematowicz <s.szematowicz@gmail.com>
2022-04-02 17:01:15 +0200

    add steps

--- Ap.SlMarsRover/MarsRover.cs
+++ Ap.SlMarsRover/MarsRover.cs
@@ -1,4 +1,5 @@
-﻿using AP.SlLoggerr;
+﻿using Ap.SlMarsRover.Base;
+using AP.SlLoggerr;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,20 @@ namespace Ap.SlMarsRover
 {
     public class MarsRover
     {
+        byte bateryPower = 100;
+
         public MarsRover(Logger logger)
         {
             logger.LogInformation("Czesc tu konstruktor łazika marsjańskiego");
         }
+    }
+
+
+
+    public enum Status
+    {
+        Success,
+        Fail,
+        Waiting,
     }
 }
---
+++ Ap.SlMarsRover/SolarPanelsUnfoldingTask.cs
@@ -0,0 +1,65 @@
+﻿using Ap.SlMarsRover.Base;
+
+namespace Ap.SlMarsRover
+{
+    // Rozkłądanie paneli słonecznych 
+    // - otworz kalpe - status ok
+    // - wystawienie paneli - staus ok 
+    // - rozłożenie paneli - ok
+    // Rozkłądanie paneli słonecznych - status ok
+    public class SolarPanelsUnfoldingTask
+    {
+        private byte _requiredBateryPower = 5;
+
+        private Status _status = Status.Waiting;
+
+
+        Step[] _steps;
+
+        public SolarPanelsUnfoldingTask(Step[] steps)
+        {
+            _steps = steps;
+        }
+
+        public void Run()
+        {
+            _status = Status.Processing;
+            foreach (var step in _steps)
+            {
+                while (step.Status == Status.Success)
+                    step.DoWork();
+            }
+        }
+
+    }
+
+    // Składanie paneli słonecznych
+    //  - żłóż panel status ok
+    //  - schowanie - status ok
+    //  - zamknięcie kalpy - status ok
+    // Składanie paneli słonecznych Ok
+    public class SolarPanelsFoldingTask
+    {
+        private byte _requiredBateryPower = 5;
+
+        private Status _status = Status.Waiting;
+
+        Step[] _steps;
+
+        public SolarPanelsFoldingTask(Step[] steps)
+        {
+            _steps = steps;
+        }
+
+        public void Run()
+        {
+            _status = Status.Processing;
+            foreach (var step in _steps)
+            {
+                while(step.Status == Status.Success)
+                step.DoWork();
+            }
+        }
+    }
+
+}
