659a606d966109503a2ac09b2f6f1bccf8fbcbce
s.szematowicz <s.szematowicz@gmail.com>
2022-05-29 10:22:45 +0200

    commit 5

---
+++ Interfaces/Demo/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var dataCollection = new DataCollection(new DateTime(2022, 1, 1), new DateTime(2022, 5, 31));
+foreach (var item in dataCollection)
+{
+    Console.WriteLine(item);
+}
+
+//2022-01-01
+//2022-01-02
+//2022-01-03
+//2022-01-04
+//...
+//2022-01-31
+//2022-02-01
+//2022-02-02
+//2022-02-03
+//...
+//2022-02-28
+//2022-03-01
+//...
+//2022-03-31
+//
